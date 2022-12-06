406d24192a7e6d7d19f86d4b17ee62e1b8a8ee83
maiteboehm <117434733+maiteboehm@users.noreply.github.com>
2022-12-06 16:36:39 +0100

    Files for seats page added

--- project/website/views.py
+++ project/website/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 from flask_login import login_required, current_user
+import os
 
 views = Blueprint('views', __name__)
 
@@ -7,6 +8,41 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/seats', methods=['GET', 'POST'])
+def seats():
+    if request.method == 'GET':
+        cwd = os.getcwd()
+        directory1 = 'website'
+        directory2 = 'templates'
+        filename = 'seats.html'
+        path_dir = os.sep.join([cwd, directory1, directory2, filename])
+        with open(r'Input_Data/chartIn4.txt', 'r') as f:
+            seat_list = []
+            seat_list_new = []
+            for line in f:
+                data = line.split('\t')
+                seat_list.append(data)
+                for row in seat_list:
+                    row_new = []
+                    for seat in row:
+                        seat_new = seat.strip()
+                        row_new.append(seat_new)
+                seat_list_new.append(row_new)
+        with open(path_dir, 'w') as file:
+            file.write('{% extends "base.html" %} {% block title %}Seats{% endblock %} {% block content%} \n')
+            file.write('<h1 align="center">Seats</h1> \n')
+            for liste in seat_list_new:
+                file.write('<div>\n')
+                for symbol in liste:
+                    if symbol == ' ':
+                        file.write()
+                    else:
+                        file.write('\t<button type="button">' + symbol + '</button>\n')
+                file.write('</div>\n')
+            file.write('{% endblock %} \n') # closing endblock, has to be at the end of html document
+
+    return render_template("seats.html", user=current_user)
 
 
 
