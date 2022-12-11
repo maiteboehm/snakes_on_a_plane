4b37e343e1e11fbb19e353b89fb851e29faeee42
maiteboehm <117434733+maiteboehm@users.noreply.github.com>
2022-12-11 13:51:31 +0100

    seat layout

--- project/website/python_scripts.py
+++ project/website/python_scripts.py
@@ -9,16 +9,23 @@ def write_seats_html(seat_list_new, directory, filename='seats.html', cwd=os.get
             file.write('{% extends "base.html" %} {% block title %}Seats{% endblock %} {% block content%} \n')
             file.write('<h1 align="center">Seats</h1> \n')
             for liste in seat_list_new:
+                print(liste)
                 if liste[0] == '':
                     continue
                 else:
                     file.write('<div class="container">\n')
                     for symbol in liste:
-                        if symbol == liste[0]:
+                        if liste.index(symbol) == 0: # creates row of numbers
                             file.write('\t<span class="badge badge-light col-1">' + symbol + '</span>\n')
-                        elif symbol == 'X':
+                        #elif liste.index(symbol) == 3: # adds space for ailes
+                           # if symbol == 'X':
+                            #    file.write('\t<button type="button" class="btn btn-danger btn-lg mr-3"></button>\n')
+                            #else:
+                              #  file.write('\t<button type="button" class="btn btn-primary btn-lg active mr-3" role="button" '
+                              #     'aria-pressed="true"></button>\n')
+                        elif symbol == 'X': # creates red button for already reserved seats
                             file.write('\t<button type="button" class="btn btn-danger btn-lg"></button>\n')
-                        else:
+                        else: # creates blue responsive button for available seats
                             file.write('\t<button type="button" class="btn btn-primary btn-lg active" role="button" '
                                    'aria-pressed="true"></button>\n')
                     file.write('</div>\n')
