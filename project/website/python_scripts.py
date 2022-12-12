19aaa0ec28bc15873aef92c81e87033731b4c790
maiteboehm <117434733+maiteboehm@users.noreply.github.com>
2022-12-12 15:19:42 +0100

    minor changes

--- project/website/python_scripts.py
+++ project/website/python_scripts.py
@@ -18,11 +18,11 @@ def write_seats_html(seat_list_new, directory, filename='seats.html', cwd=os.get
                         if liste.index(symbol) == 0: # creates row of numbers
                             file.write('\t<span class="badge badge-light col-1">' + symbol + '</span>\n')
                         #elif liste.index(symbol) == 3: # adds space for ailes
-                           # if symbol == 'X':
-                            #    file.write('\t<button type="button" class="btn btn-danger btn-lg mr-3"></button>\n')
-                            #else:
-                              #  file.write('\t<button type="button" class="btn btn-primary btn-lg active mr-3" role="button" '
-                              #     'aria-pressed="true"></button>\n')
+                         #   if symbol == 'X':
+                          #      file.write('\t<button type="button" class="btn btn-danger btn-lg mr-3"></button>\n')
+                           # else:
+                            #    file.write('\t<button type="button" class="btn btn-primary btn-lg active mr-3" role="button" '
+                             #      'aria-pressed="true"></button>\n')
                         elif symbol == 'X': # creates red button for already reserved seats
                             file.write('\t<button type="button" class="btn btn-danger btn-lg"></button>\n')
                         else: # creates blue responsive button for available seats
