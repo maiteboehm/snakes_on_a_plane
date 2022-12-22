4856ad41e5700ecd0e29cd08eb333be56418511d
NastyMelon <118937388+NastyMelon@users.noreply.github.com>
2022-12-22 15:49:32 +0100

    FlightDatabase Klassenerstellung muss noch irgendwie realisiert werden, habe noch keine Zeit gefunden, die so richtig zu implementieren.

--- project/website/FlightDatabase.py
+++ project/website/FlightDatabase.py
@@ -1,16 +1,23 @@
+from flask import Flask, redirect, url_for, flash
 import flask_sqlalchemy
 import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
 from CharReader import Filename_Liste
+DB_NAME = "Flieger.db"
 
-Fliegertypen = declarative_base()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_Name}'
+
+db = SQLAlchemy(app)
+
 
 Test_Dictionary = {
-'Filename':[['A','A'],['B','B'],['C','C']],
-'Filename2':[['A','A'],['B','B'],['C','C']]
+'Filename':[[1,2],['A','A'],['B','B'],['C','C']],
+'Filename2':[[1,2],['A','A'],['B','B'],['C','C']]
 }
 #makes names for the columns of the subclasses:
 def Column_Name_Maker(Dictionary,key):
@@ -21,14 +28,17 @@ def Column_Name_Maker(Dictionary,key):
         Column_Name_Liste.append(Column_Name)
     return Column_Name_Liste
 
-for key,value in Test_Dictionary.items():
-    class Flieger(Fliegertypen):
-        def __init__(self, name):
-            self.__tablename__ = __tablename__
-            self.__tablename__ = str(key)
-        id = Column(Integer, primary_key = True, nullable = False)
-        for column_name in Column_Name_Maker(Test_Dictionary,key):
-            setattr(Flieger, column_name, Column(String))
+    class Flieger(db.Model):
+        for key, value in Test_Dictionary.items():
+            print(key)
+            __tablename__ = str(key)
+            id = db.Column(db.Integer, primary_key = True)
+            for column in Test_Dictionary[str(key)]:
+                print(column)
+
+        #for column_name in Column_Name_Maker(Test_Dictionary,key):
+        #    setattr(Flieger, column_name, Column(String))
+
 
 
 for key, item in Test_Dictionary.items():
