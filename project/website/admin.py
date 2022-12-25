1e78b285014b529eb394f8c7497b64bce208de07
MatzeSoe <117658405+MatzeSoe@users.noreply.github.com>
2022-12-26 00:37:51 +0100

    Updating Admin Area

--- project/website/admin.py
+++ project/website/admin.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import User
 
@@ -7,12 +7,19 @@ admins = Blueprint('admins', __name__)
 @admins.route('/', methods=['GET','POST'])
 @login_required
 def admin_home():
-    return render_template('admin_home.html', user= current_user)
-
+    if current_user.role == 'admin':
+        return render_template('admin_home.html', user= current_user)
+    else:
+        flash('You need do be an Admin to access!!', category='error')
+        return redirect(url_for('views.home'))
 
 @admins.route('/user', methods=['GET','POST'])
 @login_required
 def admin_user():
-    users = User.query.all()
-    return render_template('admin_user.html', user=current_user, users=users)
+    if current_user.role == 'admin':
+        users = User.query.all()
+        return render_template('admin_user.html', user=current_user, users=users)
+    else:
+        flash('You need do be an Admin to access!!', category='error')
+        return redirect(url_for('views.home'))
 
