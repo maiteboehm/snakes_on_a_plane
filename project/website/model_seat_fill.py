efe53c73053d6f95fb847a6760b790d4bab80c38
NastyMelon <118937388+NastyMelon@users.noreply.github.com>
2022-12-29 11:15:49 +0100

    Started Filling Database with Seat class

--- project/website/model_seat_fill.py
+++ project/website/model_seat_fill.py
@@ -1,5 +1,7 @@
 import string
-
+from . import db
+from flask_login import UserMixin
+from models import Seat
 from flask import Flask
 from python_scripts import Column_Name_Maker
 from flask_sqlalchemy import SQLAlchemy
@@ -27,9 +29,9 @@ for key, value in Resorted_Dictionary.items():
             for letter in str(Seat):
                 #print(letter)
                 if letter == 'X':
-                    print(letter,ind,Number_Seat)
+                    #print(letter,ind,Number_Seat)
                     letter = alphabet[Number_Seat]
-                    print(letter)
+                    #print(letter)
                     Seat_status.append(False)
 
                 elif letter in alphabet[0:13]:
@@ -48,6 +50,8 @@ for key, value in Resorted_Dictionary.items():
 #for index,value in enumerate(Seat_type):
     #print(Flight[index],value,Seat_liste[index],)
 #print(Flight,Seat_status,Seat_liste,Seat_type,Seat_row_liste)
+new_flight = Seat(flight = Flight, seat_row = Seat_row_liste, seat_column = Seat_Liste, seat_status = Seat_status, seat_type = Seat_Type)
 print(len(Flight),len(Seat_status),len(Seat_liste),len(Seat_type),len(Seat_row_liste))
-#db.session.add(new_flight)
-#db.session.commit()
+
+db.session.add(new_flight)
+db.session.commit()
