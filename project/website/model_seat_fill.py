c9a34a9409b315ef89b0331be5d61c24946c92b4
NastyMelon <118937388+NastyMelon@users.noreply.github.com>
2022-12-29 11:57:45 +0100

    Started Filling Database with Seat class

--- project/website/model_seat_fill.py
+++ project/website/model_seat_fill.py
@@ -1,15 +1,7 @@
 import string
-from . import db
-from flask_login import UserMixin
 from models import Seat
-from flask import Flask
-from python_scripts import Column_Name_Maker
-from flask_sqlalchemy import SQLAlchemy
 from CharReader import Resorted_Dictionary
-from CharReader import Test_Dictionary
-#print(Test_Dictionary)
 alphabet = list(string.ascii_uppercase)
-#print(alphabet[0:13])
 Flight = []
 Seat_row_liste = []
 Seat_type = []
@@ -18,20 +10,17 @@ Seat_status = []
 index = 1
 #print(alphabet[4:13])
 for key, value in Resorted_Dictionary.items():
-    #print(value[0])
-    #print(key,value[1:])
+
     for ind,Row in enumerate(value[1:]):
+
         for Number_Seat,Seat in enumerate(Row):
             Seat_row_liste.append(ind + 1)
-            Flight.append(index)
+            Flight.append(int(index))
             Seat_liste.append(Seat)
-            #print(Number_Seat,Seat)
+
             for letter in str(Seat):
-                #print(letter)
                 if letter == 'X':
-                    #print(letter,ind,Number_Seat)
                     letter = alphabet[Number_Seat]
-                    #print(letter)
                     Seat_status.append(False)
 
                 elif letter in alphabet[0:13]:
@@ -47,11 +36,9 @@ for key, value in Resorted_Dictionary.items():
                     Seat_type.append('Normal')
 
     index+=1
-#for index,value in enumerate(Seat_type):
-    #print(Flight[index],value,Seat_liste[index],)
-#print(Flight,Seat_status,Seat_liste,Seat_type,Seat_row_liste)
-new_flight = Seat(flight = Flight, seat_row = Seat_row_liste, seat_column = Seat_Liste, seat_status = Seat_status, seat_type = Seat_Type)
-print(len(Flight),len(Seat_status),len(Seat_liste),len(Seat_type),len(Seat_row_liste))
+for i in range(len(Flight)):
+    New_flight = Seat(flight = Flight[i], seat_row = Seat_row_liste[i], seat_column = Seat_liste[i], seat_status = Seat_status[i], seat_type = Seat_type[i])
+    db.session.add(New_flight)
+#print(len(Flight),len(Seat_status),len(Seat_liste),len(Seat_type),len(Seat_row_liste))
 
-db.session.add(new_flight)
 db.session.commit()
