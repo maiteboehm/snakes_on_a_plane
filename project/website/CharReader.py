12501ad9e0b371368f160918490e3ae161d312d3
NastyMelon <118937388+NastyMelon@users.noreply.github.com>
2022-12-30 22:51:53 +0100

    Made function with path as variable

--- project/website/CharReader.py
+++ project/website/CharReader.py
@@ -12,25 +12,28 @@ def Dictionary_Resorter(Dictionary):
     }
     for key in Dictionary:
         Temp_Liste = []
-        #print(Filename_Dictionary[key])
+
         for index,Row_List in enumerate(Dictionary[key]):
             Temp_Liste2 = []
+
             if index>9:
                 Number = str(''.join(Row_List[0:2]))
-                print(Number)
+
             else:
                 Number = str(Row_List[0])
-                #print(Number)
 
             for ind,Seat in enumerate(Row_List):
 
                 if Seat.isdigit():
                     continue
+
                 else:
-                    #print(Number,ind,Seat)
+
                     Seat_ID = ''.join([Number,Seat])
                     Temp_Liste2.append(str(Seat_ID))
+
             Temp_Liste.append(Temp_Liste2)
+
         Resorted_Dictionary.update({key:Temp_Liste})
 
     return(Resorted_Dictionary)
@@ -41,8 +44,10 @@ def Dictionary_Creater(Filepath):
 
     }
     Flight_Number = 1
+
     for Filename in os.listdir(Filepath):
         Filename_Input = []
+
         if Filename.endswith('.txt'):
             Filename_Liste.append(Filename)
             Input = open(ChartIn_Path+str(Filename),mode = 'r')
@@ -58,6 +63,7 @@ def Dictionary_Creater(Filepath):
 
                 if Line_Liste[0]=='A':
                     Line_Liste.insert(0, '0')
+
                 if Line_Liste[0]=='1' and index == 0:
                     Letter_Liste = list(string.ascii_uppercase)
                     Tmp_Liste = [str(index)]
@@ -68,10 +74,11 @@ def Dictionary_Creater(Filepath):
                     Filename_Input.append(Tmp_Liste)
 
                 Filename_Input.append(Line_Liste)
+
         Filename_Dictionary.update({Flight_Number:Filename_Input})
-        print(Flight_Number)
         Flight_Number+=1
         Resorted_Dictionary = Dictionary_Resorter(Filename_Dictionary)
+
     return(Resorted_Dictionary)
 
 #Resorted_Dictionary = Dictionary_Creater(ChartIn_Path)
--- project/website/model_seat_fill.py
+++ project/website/model_seat_fill.py
@@ -1,21 +1,24 @@
 import string
 from .models import Seat
 from . import db
-#from CharReader import Resorted_Dictionary
 
 def Seat_Identifier(Reihe):
+
     if len(Reihe)==10:
         Gang_Liste = ['C','D','F','G']
         Fenster_Liste = ['A','J']
         Normal_Liste = ['B','E','H','I']
+
     elif len(Reihe)==6:
         Gang_Liste = ['C','D']
         Fenster_Liste = ['A','F']
         Normal_Liste = ['B', 'E']
+
     elif len(Reihe)==4:
         Gang_Liste = ['A','D']
         Fenster_Liste = ['B','C']
         Normal_Liste = []
+
     return(Gang_Liste,Fenster_Liste,Normal_Liste)
 
 def model_seat_filler(Dictionary):
@@ -25,11 +28,10 @@ def model_seat_filler(Dictionary):
     Seat_type = []
     Seat_column_liste = []
     Seat_status = []
-    #print(alphabet[4:13])
+
     for key, value in Dictionary.items():
 
         for ind,Row in enumerate(value[1:]):
-            print(Row,len(Row))
             Typen_Listen = Seat_Identifier(Row)
 
             for Number_Seat,column in enumerate(Row):
@@ -38,27 +40,28 @@ def model_seat_filler(Dictionary):
                 Seat_column_liste.append(column)
 
                 for letter in str(column):
-                    print(Typen_Listen)
+
                     if letter == 'X':
                         letter = alphabet[Number_Seat]
                         Seat_status.append('False')
+
                     elif letter in alphabet[0:13]:
                         Seat_status.append('True')
 
                     if letter in Typen_Listen[0]:
                         Seat_type.append('Gang')
-                        print(Typen_Listen[0],'Gang')
+
                     elif letter in Typen_Listen[1]:
-                        print(Typen_Listen[1],'Fenster')
                         Seat_type.append('Fenster')
 
                     elif letter in Typen_Listen[2]:
                         Seat_type.append('Normal')
-                        print(Typen_Listen[2],'Normal')
 
     for i in range(len(Flight)):
+
         New_flight = Seat(flight = Flight[i], seat_row = Seat_row_liste[i], seat_column = Seat_column_liste[i], seat_status = Seat_status[i], seat_type = Seat_type[i])
         db.session.add(New_flight)
 
     db.session.commit()
+
     return (Flight,Seat_row_liste,Seat_column_liste,Seat_status,Seat_type)
