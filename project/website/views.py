8a24c76fe50090e4544f4c690071f321f180f32b
maiteboehm <117434733+maiteboehm@users.noreply.github.com>
2023-01-03 13:51:42 +0100

    changes to seat layout
    Booking and canceling seats is functional

--- project/website/views.py
+++ project/website/views.py
@@ -1,7 +1,8 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import login_required, current_user
-from .python_scripts import write_seats_html
-from .models import Seat
+#from .python_scripts import write_seats_html
+from .models import User, Seat
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -23,13 +24,42 @@ def help():
 
 
 @views.route('/flight/<int:flights>', methods=['GET','POST'])
+@login_required
 def flight(flights):
    seat_to_render = Seat.query.filter_by(flight = flights)
+   row_number = Seat.query.filter_by(flight = flights, seat_column='A').all()
    list_of_rows = []
-   for seat in seat_to_render:
-      pass
-   row_one = Seat.query.filter_by(flight = flights, seat_row = 1)
-   return render_template('seats.html', user = current_user, seat_to_render= seat_to_render, row = row_one)
+   for i in range(len(row_number)):
+       row = Seat.query.filter_by(flight = flights, seat_row = i)
+       list_of_rows.append(row)
+
+   return render_template('seats.html', user = current_user, seat_to_render = seat_to_render, list_of_rows = list_of_rows)
+
+@views.route('/flight/<int:flights>/booking-seat/<int:id>', methods=['GET', 'POST'])
+@login_required
+def booking_seat(flights, id):
+    booked_seat = Seat.query.get_or_404(id)
+    booked_seat.seat_status = 'False'
+    booked_seat.user_id = current_user.id
+    try:
+        db.session.commit()
+        flash('Seat booked succesfully!', category='success')
+    except:
+        flash('An error occured while booking your seat', category='error')
+    return redirect('/flight/'+str(flights))
+
+@views.route('/flight/<int:flights>/cancel-seat/<int:id>', methods=['GET', 'POST'])
+@login_required
+def cancel_seat(flights, id):
+    canceled_seat = Seat.query.get_or_404(id)
+    canceled_seat.seat_status = 'True'
+    canceled_seat.user_id = 'None'
+    try:
+        db.session.commit()
+        flash('Seat canceled succesfully!', category='success')
+    except:
+        flash('An error occured while canceling your seat', category='error')
+    return redirect('/flight/'+str(flights))
 
 
 #@views.route('/seats', methods=['GET', 'POST'])
