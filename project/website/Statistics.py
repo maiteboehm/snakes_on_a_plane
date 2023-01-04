9db27005130caa9ad80d556da669b974cf032dbc
NastyMelon <118937388+NastyMelon@users.noreply.github.com>
2023-01-04 11:15:38 +0100

    Made all variables with capital letters and for loop arguments small letters

---
+++ project/website/Statistics.py
@@ -0,0 +1,10 @@
+from CharReader import Dictionary_Creater
+import os
+Path = os.path.abspath(os.curdir)
+print(Path)
+Project_Path = os.path.dirname(Path)
+print(Project_Path)
+ChartIn_Path = Path +'\Input_Data\\'
+print(ChartIn_Path)
+Resorted_Dictionary = Dictionary_Creater(ChartIn_Path)
+print(Resorted_Dictionary)
