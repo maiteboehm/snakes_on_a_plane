6fc89ce5a885aadb38d85fb3248c01f50e775fbe
maiteboehm <117434733+maiteboehm@users.noreply.github.com>
2023-01-04 16:09:35 +0100

    Refactoring

--- project/website/views.py
+++ project/website/views.py
@@ -1,38 +1,42 @@
 from flask import Flask, Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import login_required, current_user
-from .models import User, Seat
+from .models import Seat
 from . import db
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/bookingsystem', methods=['GET','POST'])
+
+@views.route('/bookingsystem', methods=['GET', 'POST'])
 @login_required
 def bookingsystem():
-    Flights = Seat.query.filter_by(seat_row='1', seat_column='A').all()
-    return render_template('bookingsystem.html', user=current_user, flights= Flights)
+    flights = Seat.query.filter_by(seat_row='1', seat_column='A').all()
+    return render_template('bookingsystem.html', user=current_user, flights=flights)
 
-@views.route('/help', methods=['GET','POST'])
+
+@views.route('/help', methods=['GET', 'POST'])
 @login_required
-def help():
+def help_page():
     return render_template('help.html', user=current_user)
 
 
-@views.route('/flight/<int:flights>', methods=['GET','POST'])
+@views.route('/flight/<int:flights>', methods=['GET', 'POST'])
 @login_required
 def flight(flights):
-   seat_to_render = Seat.query.filter_by(flight = flights)
-   row_number = Seat.query.filter_by(flight = flights, seat_column='A').all()
-   list_of_rows = []
-   for i in range(len(row_number)):
-       row = Seat.query.filter_by(flight = flights, seat_row = i)
-       list_of_rows.append(row)
+    seat_to_render = Seat.query.filter_by(flight=flights)
+    row_number = Seat.query.filter_by(flight=flights, seat_column='A').all()
+    list_of_rows = []
+    for i in range(len(row_number)):
+        row = Seat.query.filter_by(flight=flights, seat_row=i)
+        list_of_rows.append(row)
 
-   return render_template('seats.html', user = current_user, seat_to_render = seat_to_render, list_of_rows = list_of_rows)
+    return render_template('seats.html', user=current_user, seat_to_render=seat_to_render, list_of_rows=list_of_rows)
+
 
 @views.route('/flight/<int:flights>/booking-seat/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -42,10 +46,11 @@ def booking_seat(flights, id):
     booked_seat.user_id = current_user.id
     try:
         db.session.commit()
-        flash('Seat booked succesfully!', category='success')
+        flash('Seat booked successfully!', category='success')
     except:
-        flash('An error occured while booking your seat', category='error')
+        flash('An error occurred while booking your seat', category='error')
     return redirect('/flight/'+str(flights))
+
 
 @views.route('/flight/<int:flights>/cancel-seat/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -55,12 +60,7 @@ def cancel_seat(flights, id):
     canceled_seat.user_id = 'None'
     try:
         db.session.commit()
-        flash('Seat canceled succesfully!', category='success')
+        flash('Seat canceled successfully!', category='success')
     except:
-        flash('An error occured while canceling your seat', category='error')
+        flash('An error occurred while canceling your seat', category='error')
     return redirect('/flight/'+str(flights))
-
-
-
-
-
