c16d611e6e1eed2255572548c03a4a4ded84ac7c
MatzeSoe <117658405+MatzeSoe@users.noreply.github.com>
2023-01-16 17:28:30 +0100

    small updates

--- project/website/auth.py
+++ project/website/auth.py
@@ -32,7 +32,6 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-
             flash('Account created!', category='success')
 
             return redirect(url_for('views.home'))
